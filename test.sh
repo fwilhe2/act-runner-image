c78705e35555a94713a139cd20abe62a50777dfc
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2023-04-23 10:38:45 +0200

    Add variants for gcc, clang, rust

--- test.sh
+++ test.sh
@@ -8,8 +8,7 @@ else
     docker build --file=Dockerfile.$VARIANT -t act-runner-image:test-$VARIANT .
 fi
 
-docker build --file=Dockerfile.$VARIANT -t act-runner-image:test-$VARIANT .
-
+./bin/act --platform ubuntu-latest=act-runner-image:test-$VARIANT --workflows test/inspect-image.yaml --pull=false
 ./bin/act --platform ubuntu-latest=act-runner-image:test-$VARIANT --workflows test/setup-kotlin.yaml --pull=false
 ./bin/act --platform ubuntu-latest=act-runner-image:test-$VARIANT --workflows test/setup-java.yaml --pull=false
 ./bin/act --platform ubuntu-latest=act-runner-image:test-$VARIANT --workflows test/setup-go.yaml --pull=false
