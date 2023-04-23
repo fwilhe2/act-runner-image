19c18be6813607c40e73842fd10606310ddac941
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2023-04-23 19:03:53 +0200

    cancel in progress

--- generate-variants.sh
+++ generate-variants.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -x
 
-declare -A toolScripts
-toolScripts[clang]=clang
-toolScripts[gcc]=gcc
-toolScripts[jvm]=java-tools
-toolScripts[rust]=rust
-
 for variant in clang gcc java-tools rust; do
     sed 's|#%%TOOL_SCRIPTS%%|\&\& bash installers/'$variant'.sh \\|g' Dockerfile > Dockerfile.$variant
 done
