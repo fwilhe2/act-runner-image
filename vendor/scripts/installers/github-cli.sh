4b9fd93e83d4ea79d174a10d8d4b3872d974ba27
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2023-09-15 21:35:34 +0200

    Update vendor scripts

--- vendor/scripts/installers/github-cli.sh
+++ vendor/scripts/installers/github-cli.sh
@@ -9,14 +9,21 @@
 source $HELPER_SCRIPTS/install.sh
 
 # Install GitHub CLI
-if [ $(uname -m) = x86_64 ]
-then
-    downloadUrl=$(get_github_package_download_url "cli/cli" "contains(\"linux\") and contains(\"amd64\") and contains(\".deb\")")
-else
-    downloadUrl=$(get_github_package_download_url "cli/cli" "contains(\"linux\") and contains(\"arm64\") and contains(\".deb\")")
-fi
+architecture=$(uname -m)
 
-download_with_retries $downloadUrl "/tmp"
-apt install /tmp/gh_*_linux_amd64.deb
+if [ "$architecture" == "x86_64" ]; then
+    echo "Running code for AMD64 (x86_64)"
+    downloadUrl=$(get_github_package_download_url "cli/cli" "contains(\"linux\") and contains(\"amd64\") and contains(\".deb\")")
+    download_with_retries $downloadUrl "/tmp"
+    apt install /tmp/gh_*_linux_amd64.deb
+elif [ "$architecture" == "aarch64" ]; then
+    echo "Running code for ARM64 (aarch64)"
+    downloadUrl=$(get_github_package_download_url "cli/cli" "contains(\"linux\") and contains(\"arm64\") and contains(\".deb\")")
+    download_with_retries $downloadUrl "/tmp"
+    apt install /tmp/gh_*_linux_arm64.deb
+else
+    echo "Unsupported architecture: $architecture"
+    exit 1
+fi
 
 invoke_tests "CLI.Tools" "GitHub CLI"
